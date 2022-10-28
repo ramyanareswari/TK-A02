0af589be591fc9998dbf938b30557b5b510c2cd9
Ivan Rabbani Cezeliano <ivanrabbanicool@gmail.com>
2022-10-28 23:00:03 +0700

    Update 1.1 - Figuring out AJAX...

--- expiry/views.py
+++ expiry/views.py
@@ -7,17 +7,17 @@ from django.core import serializers
 from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
-@login_required(login_url='auth/login/')
+@login_required(login_url='homepage/login/')
 def show_expiry(request):
-    food_data = Food_Data.objects.filter(user = request.user).order_by('-food_expired_date')
+    # food_data = Food_Data.objects.filter(user = request.user).order_by('-food_expired_date')
     context = {
-        'query_set_food': food_data,
+        # 'query_set_food': food_data,
         'username': request.user,
     }
     return render(request, "expiry.html", context)
 
 @csrf_exempt
-@login_required(login_url='auth/login/')
+@login_required(login_url='homepage/login/')
 def add_food(request):
     if request.method == 'POST':
         food_name = request.POST.get('food_name')
@@ -25,7 +25,7 @@ def add_food(request):
         food = Food_Data.objects.create(user = request.user, food_name = food_name, food_expired_date = food_expired_date)
         return JsonResponse(
             {
-                'pk': food.id,
+                'pk': food.pk,
                 'fields': {
                     'food_name': food.food_name,
                     'food_expired_date': food.food_expired_date
@@ -34,14 +34,14 @@ def add_food(request):
             status=200,
         )
 
-@login_required(login_url='auth/login/')
+@login_required(login_url='homepage/login/')
 def delete_food(request, id):
     foods = Food_Data.objects.get(user = request.user, id = id)
     foods.delete()
     return JsonResponse(status = 200)
     # return redirect('expiry:show_expiry')
 
-@login_required(login_url='auth/login/')
+@login_required(login_url='homepage/login/')
 def show_json(request):
     foods = Food_Data.objects.filter(user = request.user).order_by('-food_expired_date')
     return HttpResponse(serializers.serialize("json", foods), content_type="application/json")
