c3032d3a44709f5dcef3367a1e70e3f5d85ad209
Nikolas Reva Bellie <nikolas.reva@ui.ac.id>
2022-10-31 11:14:12 +0700

    Update send_foodwaste user model

--- send_foodwaste/models.py
+++ send_foodwaste/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Send_FoodWaste_Model(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
     name = models.CharField(max_length = 50)
     phone_number = models.IntegerField()
     address = models.TextField()
