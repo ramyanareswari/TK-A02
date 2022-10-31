f81a6aa1ea0bd634fa3bbf4619471d947da3cfb0
Ivan Rabbani Cezeliano <ivanrabbanicool@gmail.com>
2022-10-31 15:51:28 +0700

    Update 1.8 - Add Food Button is not working..?

--- expiry/urls.py
+++ expiry/urls.py
@@ -6,6 +6,6 @@ app_name = 'expiry'
 urlpatterns = [
     path('', show_expiry, name='show_expiry'),
     path('add-food/', add_food, name='add_food'),
-    path('delete-food/<int:id>', delete_food, name='delete_food'),
+    path('delete-food/<str:id>', delete_food, name='delete_food'),
     path('json/', show_json, name='show_json'),
 ]
