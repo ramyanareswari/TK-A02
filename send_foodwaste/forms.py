1b4fd995ff362c5c5d9c641e66e31f28b2c47a07
Nikolas Reva Bellie <nikolas.reva@ui.ac.id>
2022-11-02 20:52:16 +0700

    Modified models and views

--- send_foodwaste/forms.py
+++ send_foodwaste/forms.py
@@ -4,4 +4,4 @@ from send_foodwaste.models import Send_FoodWaste_Model
 class FoodwasteForm(forms.ModelForm):
     class Meta:
         model = Send_FoodWaste_Model
-        fields = ['name', 'phone_number', 'address', 'food_type', 'expiry_date', 'weight']
+        fields = ['id', 'name', 'phone_number', 'address', 'food_type', 'expiry_date', 'weight']
