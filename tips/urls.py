6abd0edb9749d962a7d60654d93b15a4000ccefa
ramyanareswari <ramya.nareswari@ui.ac.id>
2022-11-02 22:37:54 +0700

    Update: fix tips article module

--- tips/urls.py
+++ tips/urls.py
@@ -7,5 +7,6 @@ app_name = 'tips'
 urlpatterns = [
     path('', show_tips_article, name='show_tips_article'),
     path('add-article/', add_article, name='add_article'),
-    path('json/', get_article, name='get_article')
+    path('json/', get_article, name='get_article'),
+    path('<id>/', detail_article, name='detail_article'),
 ]
